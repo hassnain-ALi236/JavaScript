25c82eec09b39e1625dc5ffa0fdf245282cdfb8e
Hassnain-Ali236 <alihassnain.ah007@gmail.com>
2026-07-24 21:34:59 +0500

    More Questions

--- practice.js
+++ practice.js
@@ -14,3 +14,16 @@ console.log(i)
 //     console.log("This is not even number");
 // }
 }
+// let marks=[98,99,100,101,102,103]
+// let average=0;
+// for(let i=0;i<marks.length;i++){
+// average+=marks[i]/marks.length;
+// }
+// console.log("Average Result of the class is =", average);
+let product=[250.645,300,900,50]
+for(let i = 0; i<product.length ; i++){
+    let productoff = product[i]/10;
+    product[i]-=productoff;
+}
+
+    console.log(product);
