ee3835ce494ac36133700bfdc28e46515689bcc5
Hassnain-Ali236 <alihassnain.ah007@gmail.com>
2026-07-24 16:45:49 +0500

    Practice Questions OF loops

---
+++ practice.js
@@ -0,0 +1,16 @@
+let num= Number(prompt("Enter a number between 1 to 100"));
+let secretnumber=96;
+while(num!=secretnumber){
+num=prompt("Enter Number Again")
+// num++;
+}
+console.log("Congratulations You entered Correct Number =" ,num);
+//Question no 2
+for(let i=0;  i<=100 ; i++) {
+if(i% 2==0){
+console.log(i)
+}
+// else{
+//     console.log("This is not even number");
+// }
+}
